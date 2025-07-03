6b772dc52dc552b7620ba943cea26ae6e56218e0
py-dev-man82 <jogja.it.services@gmail.com>
2025-07-03 10:36:34 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+BOT_TOKEN        = "YOUR_TELEGRAM_BOT_TOKEN"
+ADMIN_TELEGRAM_ID = 123456789            # your Telegram user ID
+DB_PATH          = "data/db.json"
+DB_PASSPHRASE    = "YourSecurePassphrase"
