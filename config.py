a189d7a86523e07af9ebd8575774f15324f55e61
py-dev-man82 <jogja.it.services@gmail.com>
2025-07-04 12:07:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-BOT_TOKEN        = "8080382402:AAHEd9sBkj0KpHR7ZjsyVXb1jDx6u-HK-4U"
-ADMIN_TELEGRAM_ID = 7826749230            # your Telegram user ID
+BOT_TOKEN        = "8Dx6u-HK-4U"
+ADMIN_TELEGRAM_ID = 7826230            # your Telegram user ID
 DB_PATH          = "data/db.json"
 DB_PASSPHRASE    = "YourSecurePassphrase"
