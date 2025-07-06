3b25c3c9d1727c74cb0aff1cf0474c8906c13c24
py-dev-man82 <jogja.it.services@gmail.com>
2025-07-06 13:56:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
 # Your Telegram bot token
-BOT_TOKEN         = "<YOUR_BOT_TOKEN_HERE>"
+BOT_TOKEN         = "8080382402:AAHEd9sBkj0KpHR7ZjsyVXb1jDx6u-HK-4U"
 
 # Your own Telegram user ID (for admin-only access)
-ADMIN_TELEGRAM_ID = 123456789
+ADMIN_TELEGRAM_ID = 7826749230
 
 # Path to the TinyDB file
 DB_PATH           = "data/db.json"
