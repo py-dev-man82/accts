568d3471baa4c9883f87f7be8e6eac00dc352cec
py-dev-man82 <jogja.it.services@gmail.com>
2025-07-12 23:18:36 +0800

    Update testdb.py

--- testdb.py
+++ testdb.py
@@ -1,14 +1,13 @@
 from secure_db import secure_db
 
-PIN = "1122"  # Set this to your real PIN
+PIN = "1122"
 
 if not secure_db.unlock(PIN):
     print("Unlock failed! Wrong PIN or DB corrupted.")
     exit()
 
 print("Before insert:", secure_db.all("customers"))
-
 secure_db.insert("customers", {"name": "Test User"})
 print("After insert:", secure_db.all("customers"))
 
-secure_db.db.close()
+secure_db.lock()  # <-- close only when done!
