cf7bc858aef6ece83afcf017579223568cfb7538
py-dev-man82 <jogja.it.services@gmail.com>
2025-07-13 09:02:00 +0800

    Update testdb.py

--- testdb.py
+++ testdb.py
@@ -4,14 +4,8 @@ import base64
 
 with open("data/kdf_salt.bin", "rb") as f:
     salt = f.read()
-pin = "1122"  # Use exactly what you entered
-kdf = Scrypt(
-    salt=salt,
-    length=32,
-    n=2**14,
-    r=8,
-    p=1,
-)
+pin = "1234"   # Use **exactly** the PIN you set
+kdf = Scrypt(salt=salt, length=32, n=2**14, r=8, p=1)
 key = kdf.derive(pin.encode("utf-8"))
 token = base64.urlsafe_b64encode(key)
 fernet = Fernet(token)
@@ -20,4 +14,4 @@ with open("data/db.json") as f:
     raw = f.read()
 token = base64.urlsafe_b64decode(raw.encode())
 decrypted = fernet.decrypt(token)
-print(decrypted)  # Should print JSON
+print(decrypted)
